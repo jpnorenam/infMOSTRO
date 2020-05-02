9df89cbb5ead9eb4e029455b9f04c5944b461e87
jpnorenam <jpnorenam@unal.edu.co>
2020-05-02 18:39:41 -0500

    less stressing progress bar for append_data

--- python/pyinfmostro/historic_data.py
+++ python/pyinfmostro/historic_data.py
@@ -86,10 +86,11 @@ def preprocess_data(host, start_date, end_date, xsection_name, datacarril_list, 
             with open(res_path, 'w') as res_file:
                 json.dump(res, res_file)
 
-        if (datetime_1.strftime('%Y/%m/%d') != datetime_0.strftime('%Y/%m/%d')):
-            input_dir = 'tmp/{}'.format(datetime_0.strftime('%Y/%m/%d'))
-            df_date = colapse_csv(input_dir, datacarril_list, csv_path)
+        if (datetime_1.strftime('%Y/%m/%d H') != datetime_0.strftime('%Y/%m/%d H')):
             timeline_progress(mktime_1 - init_mktime, total_timeline, prefix = '[pyinfmostro {}] timeline data preprocessed:'.format(xsection_name), suffix = 'completed', length = 50)
-        datetime_0 = datetime_1
+            if (datetime_1.strftime('%Y/%m/%d') != datetime_0.strftime('%Y/%m/%d')):
+                input_dir = 'tmp/{}'.format(datetime_0.strftime('%Y/%m/%d'))
+                df_date = colapse_csv(input_dir, datacarril_list, csv_path) 
+            datetime_0 = datetime_1
     print('[pyinfmostro {}] {} was correctly modified'.format(xsection_name, csv_path))
 
